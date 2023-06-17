2d7cea9dd57041757337a0b14877a64ca4e8d6ed
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-06-17 18:33:27 -0300

    feat: create section title structure

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,9 +1,10 @@
+import { SectionTitle } from "../sectionTitle/section-title"
 import "./experience.scss"
 
 export function Experience(){
   return(
     <div className="experience">
-        <h3>Experience</h3>
+        <SectionTitle text={"Experience"} />
         <p>Programming student and consumer of web tutorials.</p>
       <div className="experience-time">
       </div>
