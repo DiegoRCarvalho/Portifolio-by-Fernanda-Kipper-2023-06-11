f2d4fd4d6785a67dc0f207b34a6891af82596c6c
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-06-17 18:21:39 -0300

    refactor: create experience component

---
+++ src/app/components/experience/experience.tsx
@@ -0,0 +1,12 @@
+import "./experience.scss"
+
+export function Experience(){
+  return(
+    <div className="experience">
+        <h3>Experience</h3>
+        <p>Programming student and consumer of web tutorials.</p>
+      <div className="experience-time">
+      </div>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Experience } from "./components/experience/experience";
 import { Header } from "./components/header/header";
 import "./styles/home.scss"
 
@@ -5,13 +6,7 @@ export default function Home() {
   return (
     <main className="container">
       <Header/>
-      <div className="experience">
-        <h3>Experience</h3>
-        <p>Programming student and consumer of web tutorials.</p>
-      </div>
-      <div className="experience-time">
-
-      </div>
+      <Experience/>
       <div className="infos">
         <h3>Languages</h3>
         <div className="languages-info">
@@ -23,12 +18,12 @@ export default function Home() {
           <span>🎓</span>
           <span>Internet Systems degree - Universidade Estácio de Sá</span>
         </div>
-        <div className="buttons">
-          <div className="social">
+      </div>
+      <div className="buttons">
+        <div className="social">
 
-          </div>
-          <button>contact me</button>
         </div>
+        <button>contact me</button>
       </div>
     </main>
   )
