59e6a36fee6f52b9e5464ef7b3cc3ca40de9564a
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-06-19 19:17:18 -0300

    feat: create information component

--- src/app/components/information/information.tsx
+++ src/app/components/information/information.tsx
@@ -1,14 +1,15 @@
+import { SectionTitle } from "../sectionTitle/section-title"
 import "./information.scss"
 
 export function Information(){
   return(
-    <div className="infos">
-      <h3>Languages</h3>
+    <div className="information">
+      <SectionTitle text="Languages"/>
       <div className="languages-info">
         <span>🇺🇸 EN - Intermediary</span>
         <span>🇧🇷 PT-BR - Native Speaker</span>
       </div>
-      <h3>Education</h3>
+      <SectionTitle text="Education"/>
       <div className="educational-info">
         <span>🎓</span>
         <span>Internet Systems degree - Universidade Estácio de Sá</span>
